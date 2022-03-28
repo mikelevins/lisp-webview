e6a5e237edeba54762447b54b5fc02c6b48efa51
mikel evins <mikel@evins.net>
2022-03-28 10:09:37 -0500

    enable debugging tools (on platforms that support them)

--- platform/macos/version.c
+++ platform/macos/version.c
@@ -10,7 +10,7 @@ extern "C" {
   int patch_version() { return 2; }
 
   webview_t testwin(){
-    webview_t w = webview_create(0, NULL);
+    webview_t w = webview_create(1, NULL); // nonzero first argument means enable development tools in the webview
     webview_set_title(w, "Webview Test");
     webview_set_size(w, 480, 320, WEBVIEW_HINT_NONE);
     webview_navigate(w, "https://en.m.wikipedia.org/wiki/Main_Page");
