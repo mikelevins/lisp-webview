2361d5ab44111bedd7f432599246dfb32d994434
mikel evins <mikel@evins.net>
2022-03-29 17:23:21 -0500

    added code and makefile for Fedora 35 build

---
+++ platform/linux/test/main.cc
@@ -0,0 +1,15 @@
+
+#include "webview.h"
+#ifdef WIN32
+int WINAPI WinMain(HINSTANCE hInt, HINSTANCE hPrevInst, LPSTR lpCmdLine,
+                   int nCmdShow) {
+#else
+int main() {
+#endif
+  webview::webview w(true, nullptr);
+  w.set_title("Minimal example");
+  w.set_size(480, 320, WEBVIEW_HINT_NONE);
+  w.navigate("https://en.m.wikipedia.org/wiki/Main_Page");
+  w.run();
+  return 0;
+}
