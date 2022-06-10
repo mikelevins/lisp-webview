e2b9507f52a320265c56e18c982dfe2feaeca99f
mikel evins <mikel@evins.net>
2022-06-10 09:24:09 -0500

    export webview API in the library

--- platform/linux/ubuntu/version.c
+++ platform/linux/ubuntu/version.c
@@ -9,14 +9,28 @@ extern "C" {
   int minor_version() { return 0; }
   int patch_version() { return 2; }
 
-  webview_t testwin(){
-    webview_t w = webview_create(1, NULL); // nonzero first argument means enable development tools in the webview
-    webview_set_title(w, "Webview Test");
-    webview_set_size(w, 480, 320, WEBVIEW_HINT_NONE);
-    webview_navigate(w, "https://en.m.wikipedia.org/wiki/Main_Page");
-    webview_run(w);
-    return 0;
+
+  WEBVIEW_API webview_t wv_create(int debug, void *window){
+    webview_t w;
+    w = webview_create(debug, window);
+    return w;
   }
+
+  WEBVIEW_API void wv_destroy(webview_t w){ webview_destroy(w); }
+  WEBVIEW_API void wv_run(webview_t w){ webview_run(w); }
+  WEBVIEW_API void wv_terminate(webview_t w) { webview_terminate(w); }
+
+  WEBVIEW_API void *wv_get_window(webview_t w){
+    void *win=webview_get_window(w);
+    return win;
+  }
+
+  WEBVIEW_API void wv_set_title(webview_t w, const char *title){ webview_set_title(w, title); }
+  WEBVIEW_API void wv_set_size(webview_t w, int width, int height, int hints){ webview_set_size(w,width,height,hints); }
+  WEBVIEW_API void wv_navigate(webview_t w, const char *url){ webview_navigate(w,url); }
+  WEBVIEW_API void wv_set_html(webview_t w, const char *html){ webview_set_html(w,html); }
+  WEBVIEW_API void wv_init(webview_t w, const char *js){ webview_init(w,js); }
+  WEBVIEW_API void wv_eval(webview_t w, const char *js){ webview_eval(w,js); }
   
 #ifdef __cplusplus
 }
--- platform/linux/ubuntu/version.h
+++ platform/linux/ubuntu/version.h
@@ -7,6 +7,18 @@ extern "C" {
   int patch_version();
   webview_t testwin();
 
+  webview_t wv_create(int debug, void *window);
+  void wv_destroy(webview_t window);
+  void wv_run(webview_t window);
+  void wv_terminate(webview_t window);
+  void *wv_get_window(webview_t window);
+  void wv_set_title(webview_t window, const char *title);
+  void wv_set_size(webview_t window, int width, int height, int hints);
+  void wv_navigate(webview_t window, const char *url);
+  void wv_set_html(webview_t window, const char *html);
+  void wv_init(webview_t window, const char *js);
+  void wv_eval(webview_t window, const char *js);
+
 #ifdef __cplusplus
 }
 #endif
